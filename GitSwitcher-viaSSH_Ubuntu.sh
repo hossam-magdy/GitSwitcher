5a50190f0a6f4018830a705d92cab53075de9267
Hossam Magdy <hossam6236@gmail.com>
2017-06-30 17:34:49 +0200

    Add comments for future SSH improvements

--- GitSwitcher-viaSSH_Ubuntu.sh
+++ GitSwitcher-viaSSH_Ubuntu.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-ssh_ip='REMOTE_IP'
-ssh_user='SSH_USER'
-ssh_pass='SSH_PASS'
-commands_file=$(dirname "$0")'/GitSwitcher.sh'
+ssh_ip='REMOTE_IP';
+ssh_user='SSH_USER';
+ssh_pass='SSH_PASS';
+commands_file=$(dirname "$0")'/GitSwitcher.sh';
 
-command -v sshpass >/dev/null 2>&1 || { sudo apt-get install sshpass; }
+# https://packages.ubuntu.com/xenial/amd64/sshpass/download
+command -v sshpass >/dev/null 2>&1 || { sudo apt-get install sshpass; };
+# plink http://manpages.ubuntu.com/manpages/wily/man1/plink.1.html
 
-sshpass -p ${ssh_pass} ssh ${ssh_user}@${ssh_ip} 'bash -s' < ${commands_file}
+sshpass -p ${ssh_pass} ssh ${ssh_user}@${ssh_ip} 'bash -s' < ${commands_file};
 
-read;
+read -n 1 -s;
