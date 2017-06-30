03eddaa2497938c70c3efca67f69458486b2ad35
Hossam Magdy <hossam6236@gmail.com>
2017-06-30 17:41:50 +0200

    Add comments for future SSH improvements

--- GitSwitcher-viaSSH_Ubuntu.sh
+++ GitSwitcher-viaSSH_Ubuntu.sh
@@ -5,9 +5,9 @@ ssh_user='SSH_USER';
 ssh_pass='SSH_PASS';
 commands_file=$(dirname "$0")'/GitSwitcher.sh';
 
-# https://packages.ubuntu.com/xenial/amd64/sshpass/download
+# sshpass:  http://manpages.ubuntu.com/manpages/wily/man1/sshpass.1.html    https://packages.ubuntu.com/xenial/amd64/sshpass/download
+# plink:    http://manpages.ubuntu.com/manpages/wily/man1/plink.1.html      https://packages.ubuntu.com/xenial/amd64/plink/download
 command -v sshpass >/dev/null 2>&1 || { sudo apt-get install sshpass; };
-# plink http://manpages.ubuntu.com/manpages/wily/man1/plink.1.html
 
 sshpass -p ${ssh_pass} ssh ${ssh_user}@${ssh_ip} 'bash -s' < ${commands_file};
 
--- GitSwitcher-viaSSH_Windows.bat
+++ GitSwitcher-viaSSH_Windows.bat
@@ -5,11 +5,9 @@ SET ssh_user=SSH_USER
 SET ssh_pass=SSH_PASS
 SET commands_file=%~dp0\GitSwitcher.sh
 
-SET DOWNLOAD_FROM=https://the.earth.li/~sgtatham/putty/latest/w64/putty.exe
+SET url_plink=https://the.earth.li/~sgtatham/putty/latest/w64/putty.exe
 WHERE putty >NUL 2>&1
-IF %ERRORLEVEL% NEQ 0 (
-    powershell -command "curl -outf \"%~dp0\putty.exe\" \"%DOWNLOAD_FROM%\""
-)
+IF %ERRORLEVEL% NEQ 0 powershell -command "curl -outf \"%~dp0\putty.exe\" \"%url_plink%\""
 
 putty -t -ssh %ssh_user%@%ssh_ip% -pw %ssh_pass% -m "%commands_file%"
 
