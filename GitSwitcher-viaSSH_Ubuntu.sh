1de79b43532d32ed5f4ef1c29bd677c7a3b90cce
Hossam Magdy <hossam6236@gmail.com>
2017-06-30 17:22:13 +0200

    Improve SSH login scripts

--- GitSwitcher-viaSSH_Ubuntu.sh
+++ GitSwitcher-viaSSH_Ubuntu.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-#sudo apt-get install sshpass;
-commands_file=$(dirname "$0")'/GitSwitcher.sh'
 ssh_ip='REMOTE_IP'
 ssh_user='SSH_USER'
 ssh_pass='SSH_PASS'
+commands_file=$(dirname "$0")'/GitSwitcher.sh'
+
+command -v sshpass >/dev/null 2>&1 || { sudo apt-get install sshpass; }
 
 sshpass -p ${ssh_pass} ssh ${ssh_user}@${ssh_ip} 'bash -s' < ${commands_file}
+
 read;
--- GitSwitcher-viaSSH_Windows.bat
+++ GitSwitcher-viaSSH_Windows.bat
@@ -1,7 +1,16 @@
-@echo off
+@ECHO OFF
 
-:: https://the.earth.li/~sgtatham/putty/latest/w64/putty.exe
+SET ssh_ip=REMOTE_IP
+SET ssh_user=SSH_USER
+SET ssh_pass=SSH_PASS
+SET commands_file=%~dp0\GitSwitcher.sh
 
-putty -t -ssh SSH_USER@REMOTE_IP -pw SSH_PASS -m GitSwitcher.sh
+SET DOWNLOAD_FROM=https://the.earth.li/~sgtatham/putty/latest/w64/putty.exe
+WHERE putty >NUL 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    powershell -command "curl -outf \"%~dp0\putty.exe\" \"%DOWNLOAD_FROM%\""
+)
 
-pause
+putty -t -ssh %ssh_user%@%ssh_ip% -pw %ssh_pass% -m "%commands_file%"
+
+PAUSE
